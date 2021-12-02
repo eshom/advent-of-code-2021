177c1b464e49b569497d959a3e313cc5cf63e3da
eshom <48328872+eshom@users.noreply.github.com>
2021-12-02 19:57:41 +0200

    full solution day 2

--- day2/day2.R
+++ day2/day2.R
@@ -12,39 +12,49 @@ forward 2", col.names = cols))
 
 ## Puzzle one
 
-## position object: a vector of length 2 with horizontal / depth elements
-position <- c(horizontal = 0, depth = 0)
+forward <- function(arg, pos, with_aim = FALSE) {
+        if (!with_aim)
+                return(pos + c(arg, 0))
 
-forward <- function(arg, pos) {
-        pos + c(arg, 0)
+        pos + c(arg, pos[3] * arg, 0)
 }
 
-down <- function(arg, pos) {
-        pos + c(0, arg)
+down <- function(arg, pos, with_aim = FALSE) {
+        if (!with_aim)
+                return(pos + c(0, arg))
+
+        pos + c(0, 0, arg)
 }
 
-up <- function(arg, pos) {
-        pos + c(0, -arg)
+up <- function(arg, pos, with_aim = FALSE) {
+        if (!with_aim)
+                return(pos + c(0, -arg))
+
+        pos + c(0, 0, -arg)
 }
 
-## command object, a data.frame/list with 1 row and 2 columns: command / arg elements
-run_command <- function(command, pos) {
+## Works for both versions of `pos` without branching!
+run_command <- function(command, pos, with_aim = FALSE) {
         comm <- command$command
         arg <- command$arg
+
         switch(comm,
-               forward = forward(arg, pos),
-               down = down(arg, pos),
-               up = up(arg, pos))
+               forward = forward(arg, pos, with_aim),
+               down = down(arg, pos, with_aim),
+               up = up(arg, pos, with_aim))
 }
 
-start_course <- function(all_commands) {
+## Also no branching here
+start_course <- function(all_commands, with_aim = FALSE) {
         ncommands <- nrow(all_commands)
 
-        pos_out <- matrix(0, ncol = 2, nrow = ncommands)
+        pos_n_elements <- with_aim + 2 # results in 2 if FALSE or 3 if TRUE
 
-        pos_out[1, ] <- run_command(all_commands[1, ], c(0, 0))
+        pos_out <- matrix(0, ncol = pos_n_elements, nrow = ncommands)
+
+        pos_out[1, ] <- run_command(all_commands[1, ], vector("numeric", length = pos_n_elements), with_aim)
         for (i in seq_len(ncommands)[-1]) {
-                pos_out[i, ] <- run_command(all_commands[i, ], pos_out[i - 1, ])
+                pos_out[i, ] <- run_command(all_commands[i, ], pos_out[i - 1, ], with_aim)
         }
 
         pos_out
@@ -52,3 +62,10 @@ start_course <- function(all_commands) {
 
 read_input() |> start_course() |> tail(1) |> prod()
 read_input(example = FALSE) |> start_course() |> tail(1) |> prod()
+
+## Puzzle two
+read_input() |> start_course(with_aim = TRUE) |> tail(1) -> result
+prod(result[-3])
+
+read_input(example = FALSE) |> start_course(with_aim = TRUE) |> tail(1) -> result
+prod(result[-3])
